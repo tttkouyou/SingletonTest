815db05104c094c779f8a54dea3e83f0388a0284
tttkouyou <bjkt1290248@gn.iwasaki.ac.jp>
2021-05-20 13:59:12 +0900

    SingletonTest.javaを追加しました

---
+++ SingletonTest.java
@@ -0,0 +1,36 @@
+
+public class SingletonTest {
+
+	public static void main(String[] args) {
+		Renban renbanA = Renban.getInstance();
+		Renban renbanB = Renban.getInstance();
+
+		System.out.println(renbanA.getNumber());
+		System.out.println(renbanB.getNumber());
+		System.out.println(renbanB.getNumber());
+		System.out.println(renbanA.getNumber());
+		System.out.println(renbanB.getNumber());
+
+
+	}
+
+}
+
+class Renban {
+	private static Renban instance  = new Renban();
+	private int number;
+
+	private Renban() {
+		number = 0;
+	}
+
+	public static Renban getInstance(){
+		return instance;
+	}
+
+	public String getNumber() {
+		number = number + 1;
+		String str = String.format("%04d", number);
+		return str;
+	}
+}
